860792ced7598a930e0084d8c804f22938aff96d
Sheleg <delmaler1@gmail.com>
2021-12-28 23:33:12 +0200

    added widgets

--- lib/history.dart
+++ lib/history.dart
@@ -1,33 +1,26 @@
 import 'package:flutter/material.dart';
 import 'main.dart';
-import 'package:ssh/ssh.dart';
 
-var client = SSHClient(
-  host: "pi@zsheleg",
-  port: 22,
-  username: "zsheleg",
-  passwordOrKey: "2003",
-);
-
-class ConnectMenu extends StatelessWidget {
+class HistoryMenu extends StatelessWidget {
   //can be stateful need to learn more
-  const ConnectMenu({Key? key}) : super(key: key);
+  const HistoryMenu({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Connect Menu1"),
+          title: const Text("History menu"),
         ),
         body: Column(children: [
           Center(
             child: ElevatedButton(
-              onPressed:
-                  null /* () async {
-                await client.connect();
-              },*/
-              ,
-              child: const Text('Connect to Raspberry Pi1'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => WireColur()),
+                );
+              },
+              child: const Text('Last Colur'),
             ),
           ),
           Center(
